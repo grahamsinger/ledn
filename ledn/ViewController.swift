19e1f1debf99c419fcde51d2cc6385fbbec4b209
Graham Singer <grahamtsinger@gmail.com>
2017-01-08 18:02:18 -0600

    Initial Commit

---
+++ ledn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ledn
+//
+//  Created by Graham Singer on 1/8/17.
+//  Copyright © 2017 Graham Singer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
